8c2c482625e348474ba786d8fded31d2d8f5634e
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-10-08 16:55:16 -0700

    added java script

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const answerFly = document.querySelector("#answerf")
 inputwalk.addEventListener("input", calculateWalk)
 //---------------------------------------------------------------------------
 function calculateWalk() {
-
+//calculate walk takes in the miles distance of the equator and divides it by the inputed mph walking speed and divided by 24 to get days  
 
 const walkAround = parseInt((24901/inputwalk.value)/24)
 console.log(walkAround, "walkAround")
@@ -24,6 +24,7 @@ calculateWalk()
 //----------------------------------------------------------------------------
 
 inputdrive.addEventListener('input', calculateDrive)
+//Calculate drives takes in the driving speed mph to calculate how long it would take to drive the length of the equator
 
 function calculateDrive() {
 
@@ -38,15 +39,20 @@ function calculateDrive() {
     
     //----------------------------------------------------------------
 
-    //inputfly.addEventListener ('select', calculatefly)
 
+//this drop down list would return a string value based on which plane was selected
 function btn001() {
-
+   
   document.getElementById("answerf").innerHTML = "On a comercial flight: 2 days"
+  
 }
+
+
 
 function btn002() {
    
-  document.getElementById("answerf").innerHTML = "On jet x-15 flight: 6 hours!"
+  document.getElementById("answerf").innerHTML = "On a jet x-15 flight: 6 hours!"
+  
 }
-
+btn001()
+btn002()
